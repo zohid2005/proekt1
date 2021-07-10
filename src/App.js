9b3982b81a2d9806abb04fb34f7d82f9cde2058a
zohid2005 <nigmatullayevzohidjon9@gmail.com>
2021-07-10 17:38:34 +0500

    body ozgardi

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.png';
+
 import './App.css';
 
 import Card from "./Card"
@@ -25,20 +25,6 @@ const data = [
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
       <div className="container">
         <div className="row mt-5">
           <div className="col-md-4">
